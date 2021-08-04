26ecec4fd277ca925172949047037694148d9ff4
johnas <johnas@gmail.com>
2021-08-04 16:14:44 +0700

    f

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(logger("dev"));
 // const dbPath = "mongodb://localhost/socketChatDB";
 
 
-const dbPath="mongodb+srv://0977361506:0977361506@cluster0.kzuv8.mongodb.net/socketChatDB?retryWrites=true&w=majority";
+ const dbPath="mongodb+srv://0977361506:0977361506@cluster0.kzuv8.mongodb.net/socketChatDB?retryWrites=true&w=majority";
 
 
 // const dbPath = `mongodb://github_demo:Pass\#12@ds149511.mlab.com:49511/socketionodejschat`;
